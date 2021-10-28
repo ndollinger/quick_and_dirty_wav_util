19d5137bf2b52f53f6600eaee6758522134d2390
Nick Dollinger <ndollinger@gmail.com>
2021-10-27 20:50:18 -0500

    Add Better docstring for wave_channel

--- wave_channel.py
+++ wave_channel.py
@@ -1,13 +1,36 @@
+"""Classes and Method to evaluate and manipulate the channels of wave files.
+
+Classes:
+
+    wav_channel_reader
+"""
+
 import wave
 
 class wav_channel_reader:
+    """Wave Channel Reader.
+
+    Act as an interface to the sound bytes of a single channel of a wav file.    
+
+    Methods:
+        __iter__ - allows for iteration through the bytes of the channel of the wave file.
+    """
     
     def __init__(self, wave_read: wave.Wave_read, channel):
-        """YEAH."""
+        """Create a new wave_channel_reader.
+        
+        Parameters:
+            wave_read - a Wave_read object
+            channel - passed int idicating which audio channel this obj repr.
+
+        Raises:
+            ValueError - Raised if the "channel" parameter indicates a channel that doesn't exist in the passed Wave_read object.
+
+        """
         self.audio_bytes = bytearray(wave_read.readframes(wave_read.getnframes()))
         self.num_channels = wave_read.getnchannels()
         self.bytes_per_sample = wave_read.getsampwidth()
-        self.channel = channel # Which of the channels in the wave is going to be _this_ channel
+        self.channel = channel # Set which of the channels in the wave is going to be _this_ channel
         if(channel in range(0, wave_read.getnchannels())):
             self.channel = channel
         else:
@@ -15,15 +38,18 @@ class wav_channel_reader:
 
     def __iter__(self):
         """Yield the next byte from the channel."""
-        # for index in range(self.channel - 1, len(self.audio_bytes), int((self.num_channels * self.bytes_per_sample))):
-        for index in range(self.channel - 1, len(self.audio_bytes), 8):
 
-            # mono_sample = self.audio_bytes[index:int(index+(self.bytes_per_sample/self.num_channels)+1)]
-            mono_sample = self.audio_bytes[int(index):int(index+(self.bytes_per_sample/self.num_channels)+1)]
+        # Wav file references
+        # https://wavefilegem.com/how_wave_files_work.html
+
+        # The data is the individual samples. An individual sample is the bit size times the number of channels. 
+        # For example, a monaural (single channel), eight bit recording has an individual sample size of 8 bits. 
+        # A monaural sixteen-bit recording has an individual sample size of 16 bits. 
+        # A stereo sixteen-bit recording has an individual sample size of 32 bits.
+
+        # Samples are placed end-to-end to form the data. 
+        # So, for example, if you have four samples (s1, s2, s3, s4) then the data would look like: s1s2s3s4.
         
+        for index in range(self.channel - 1, len(self.audio_bytes), 8):
+            mono_sample = self.audio_bytes[int(index):int(index+(self.bytes_per_sample/self.num_channels)+1)]
             yield mono_sample
-
-    # def __next__(self):
-    #     x = self.a
-    #     self.a += 1
-    #     return x
