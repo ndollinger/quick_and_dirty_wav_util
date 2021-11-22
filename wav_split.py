2da2ae75710a378c1926d19bf0d96e11ddc32069
Nick Dollinger <ndollinger@gmail.com>
2021-11-22 15:41:23 -0600

    pep8 fixes

--- wav_split.py
+++ wav_split.py
@@ -5,67 +5,84 @@ Converts a Stereo wav into a mono wav.
 One channel is discarded
 
 """
-#!/usr/bin/env python3
 
 import sys
 import wave
-import uuid
 import argparse
 import os
 from wave_channel import convert_wav_to_mono
+
 
 def main(wav_file_name, output_directory, output_file_name, channel, verbose):
     """Split the Wave."""
     try:
         wf = wave.open(wav_file_name, "rb")
     except wave.Error as wave_except:
-        print(f'Error Opening {wav_file_name} : {str(wave_except)}', file=sys.stderr)
+        print(f'Error Opening {wav_file_name} : {str(wave_except)}',
+              file=sys.stderr)
     # check for non-mono wavs and convert them, else output message
     if can_be_split(wf):
-        convert_wav_to_mono(wf, output_directory, output_file_name, channel, verbose)
-        print ("Converted to " + output_file_name)
+        convert_wav_to_mono(wf, output_directory, output_file_name, channel,
+                            verbose)
+        print("Converted to " + output_file_name)
     else:
-        print (wav_file_name + " wav is already mono")
+        print(wav_file_name + " wav is already mono")
     wf.close
+
 
 def can_be_split(wf: wave.Wave_read) -> bool:
     """Determine if a Wave_read object can be split into multiple channels."""
-    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    if (
+        wf.getnchannels() != 1
+        or wf.getsampwidth() != 2
+        or wf.getcomptype() != "NONE"
+    ):
         return True
     else:
         return False
+
 
 def create_output_dir(output_dir: str):
     """Create a dir to output generated waves if it doesn't already exist."""
     try:
         os.mkdir(output_dir)
     except FileExistsError:
-        pass # Suppress the exception
+        pass  # Suppress the exception
+
 
 if __name__ == '__main__':
     default_output_dir = "generated_waves"
-    parser = argparse.ArgumentParser(description='Create a Mono .wav from a Stereo .wav')
-    parser.add_argument("wav_file", 
+    parser = argparse.ArgumentParser(
+        description="Create a Mono .wav from Stereo.wav")
+    parser.add_argument("wav_file",
                         help="File Name of Wav file to translate",
                         type=str)
     parser.add_argument("--output_directory",
-                       help="Directory where split wave files end up",
-                       type=str, 
-                       default=default_output_dir)
+                        help="Directory where split wave files end up",
+                        type=str,
+                        default=default_output_dir)
     parser.add_argument("--output_file_name",
                         "-o",
-                        help="File name of the newly created mono wave file.  If not supplied, the output file is the input file name with the channel appended to the file name.",
+                        help="File name of the new mono wave file." +
+                        "If not supplied, the output file name is " +
+                        "in the name of the input file with the " +
+                        "channel name appended",
                         type=str)
     parser.add_argument("--channel",
-                       help="which channel to keep",
-                       type=int, 
-                       default=0)
-    parser.add_argument('--verbose', 
-                        '-v', 
-                        action='count', 
+                        help="which channel to keep",
+                        type=int,
+                        default=0)
+    parser.add_argument('--verbose',
+                        '-v',
+                        action='count',
                         default=0,
                         help="prints additional details to stdout")
     args = parser.parse_args()
     create_output_dir(args.output_directory)
-    output_file_name = args.output_file_name or f'{os.path.basename(args.wav_file).split(".wav")[0]}_channel_{args.channel}.wav'
-    main(args.wav_file, args.output_directory, output_file_name, args.channel, args.verbose)
+    output_file_name = (
+        args.output_file_name
+        or f'{os.path.basename(args.wav_file).split(".wav")[0]}\
+            _channel_{args.channel}.wav'
+    )
+    main(args.wav_file, args.output_directory,
+         output_file_name, args.channel, args.verbose)
--- wave_channel.py
+++ wave_channel.py
@@ -1,115 +1,151 @@
 """Functions to evaluate and manipulate the channels of wave files.
 
 Functions:
-    convert_wave_to_mono: creates a new wave file with just one channel out of the passed wave.Wave_read files
+    convert_wave_to_mono: creates a new wave file with
+    just one channel out of the passed wave.Wave_read files
     get_one_channel: returns one channel of audio bytes
-    read_wave_channel: return an iterator to the bytes of the audio frames in the wav file
+    read_wave_channel: return an iterator to the bytes of
+    the audio frames in the wav file
 Classes:
-    InvalidWaveChannel: Exception thrown when an attempt is made to access a channel the doesn't exist in the wave.Wave_read object
+    InvalidWaveChannel: Exception thrown when an attempt
+    is made to access a channel the doesn't exist in the
+    wave.Wave_read object
 """
 
-from typing import Generator
 import wave
 from time import perf_counter
 import os
 
-def convert_wav_to_mono(wave_file:wave.Wave_read, output_directory: str, mono_wave_file_name: str, channel_num: int, verbose: int):
+
+def convert_wav_to_mono(wave_file: wave.Wave_read,
+                        output_directory: str,
+                        mono_wave_file_name: str,
+                        channel_num: int, verbose: int):
     """
-    Create a new wave file on disk from one of the channels of the passed Wave_read object.
+    Create a new wave file on disk from one of the channels\
+    of the passed Wave_read object.
 
     Args:
-        wave_file: a wave.Wave_read object that will have one audio channel read into a new file.  This object will not be manipulated
-        output_directory: The directory on disk where the newly created mono wave file will be stored.
+        wave_file: a wave.Wave_read object that will have one audio
+        channel read into a new file.  This object will not be manipulated
+        output_directory: The directory on disk where the newly created
+        mono wave file will be stored.
         mono_wave_file_name: name to use for the newly created mono wave file
-        channel_num: the channel id of the channel that will be read into the new wave file
-        verbose: if set, output additional information about the files and split time
+        channel_num: the channel id of the channel that will be
+        read into the new wave file
+        verbose: if set, output additional information about the
+        files and split time
 
     Returns:
-        The name of the newly created wave file # TODO this doesn't really make sense
+        The name of the newly created wave file # TODO this doesn't really\
+        make sense
     """
     # only going to keep one of the channels
     if verbose:
-        print(f'There are {wave_file.getsampwidth()} bytes per sample in this wave')
+        print(f'There are {wave_file.getsampwidth()} \
+              bytes per sample in this wave')
         print(f'The sampling frequency is {wave_file.getframerate()}')
 
     # TODO: Make it configurable which channel to keep
 
-    # This is an extremely large `try` block, but once the `wave.open` runs it will create a new file on disk which we don't want to be there in the case something goes wrong writing the bytes etc.
+    # This is an extremely large `try` block, but once the `wave.open` runs
+    # it will create a new file on disk which we don't want to be there
+    # in the case something goes wrong writing the bytes etc.
     try:
-        mono_wave = wave.open(f'{output_directory}/{mono_wave_file_name}',"wb")
+        mono_wave = wave.open(
+                    f'{output_directory}/{mono_wave_file_name}', "wb")
         params = wave_file.getparams()
         mono_wave.setparams(params)
         mono_wave.setnchannels(1)
-    
+
         # Keep track of time required to make the conversion
         start_time = perf_counter()
- 
+
         mono_wave_bytes = get_one_channel(wave_file, channel_num)
 
         mono_wave.writeframesraw(mono_wave_bytes)
-        mono_wave.close() # done writing to this thing, have to create a Wave_read object later
-    except:
+        mono_wave.close()
+    except Exception:
         os.remove(f'{output_directory}/{mono_wave_file_name}')
         raise
 
-    if verbose: print("Conversion completed in: ", perf_counter() - start_time, "seconds")
+    if verbose:
+        print(f'Conversion completed in: {perf_counter() - start_time}seconds')
 
     return mono_wave_file_name
+
 
 def get_one_channel(wave_file: wave.Wave_read, channel_num=1) -> bytearray:
     """
     Split out one channel of audio data from the passed Wave_read object.
 
     Args:
-        wave_file: a wave.Wave_read object that will have one audio channel read into a new file.  This object will not be manipulated
+        wave_file: a wave.Wave_read object that will have one audio
+        channel read into a new file.  This object will not be manipulated
         channel_num: Which channel's bytes to return
 
     Returns:
-        A bytearray containing the audio frams for one channel of the passed Wave_read object
+        A bytearray containing the audio frams for one channel
+        of the passed Wave_read object
     """
     mono_wave_bytes = bytearray()
     for data_bytes in read_wave_channel(wave_file, channel_num):
         mono_wave_bytes += data_bytes
-    mono_wave_bytes += b'\x01' # TODO Padding byte if M*Nc*Ns is odd, else 0
-    return mono_wave_bytes 
+    mono_wave_bytes += b'\x01'  # TODO Padding byte if M*Nc*Ns is odd, else 0
+    return mono_wave_bytes
 
-def read_wave_channel(wave_file:wave.Wave_read, read_channel_index:int):
+
+def read_wave_channel(wave_file: wave.Wave_read, read_channel_index: int):
     """
-    Yield the next() byte from channel "read_channel_index" of the passed Wave_read object.
+    Yield the next() byte from channel "read_channel_index" of the passed\
+    Wave_read object.
 
     Args:
-        wave_file: a wave.Wave_read object that will have one audio channel read into a new file.  This object will not be manipulated
+        wave_file: a wave.Wave_read object that will have one
+        audio channel read into a new file.
+        This object will not be manipulated
         read_channel_index: Which channel's bytes to return
 
     Yields:
         bytes of one channel out of the entire sample
     """
     if(read_channel_index not in range(0, wave_file.getnchannels())):
-        raise InvalidWaveChannel(f'There is no channel {read_channel_index}.  {wave_file} only contains {wave_file.getnchannels()} channels')
-    
+        raise InvalidWaveChannel(f'There is no channel {read_channel_index}.'
+                                 f'{wave_file} only contains'
+                                 f'{wave_file.getnchannels()} channels')
     audio_bytes = bytearray(wave_file.readframes(wave_file.getnframes()))
 
     # Wav file references
     # https://wavefilegem.com/how_wave_files_work.html
 
-    # The data is the individual samples. An individual sample is the bit size times the number of channels. 
-    # For example, a monaural (single channel), eight bit recording has an individual sample size of 8 bits. 
-    # A monaural sixteen-bit recording has an individual sample size of 16 bits. 
+    # The data is the individual samples. An individual sample is the bit
+    # size times the number of channels.
+    # For example, a monaural (single channel), eight bit recording has an
+    # individual sample size of 8 bits.
+    # A monaural sixteen-bit recording has an individual sample size of 16 bits
     # A stereo sixteen-bit recording has an individual sample size of 32 bits.
 
-    # Samples are placed end-to-end to form the data. 
-    # So, for example, if you have four samples (s1, s2, s3, s4) then the data would look like: s1s2s3s4.
-    # Multiple channels is like |s1ch1s1ch2|s2ch1s2ch2|s3ch1s3ch2|...    
-    # therefore, the size of all the samples for all channels is the step size to step between the groups of samples
-    size_of_one_sample_from_all_channels = wave_file.getsampwidth()*wave_file.getnchannels()
-    # if we want channel 0, start at index 0, otherwise offset the start to align with the channel wanted
+    # Samples are placed end-to-end to form the data.
+    # So, for example, if you have four samples (s1, s2, s3, s4) then the
+    # data would look like: s1s2s3s4.
+    # Multiple channels is like |s1ch1s1ch2|s2ch1s2ch2|s3ch1s3ch2|...
+    # therefore, the size of all the samples for all channels is the step
+    # size to step between the groups of samples
+    size_of_one_sample_from_all_channels = (wave_file.getsampwidth()
+                                            * wave_file.getnchannels())
+    # if we want channel 0, start at index 0,
+    # otherwise offset the start to align with the channel wanted
     first_index = read_channel_index*wave_file.getsampwidth()
 
-    for index in range(first_index, len(audio_bytes), size_of_one_sample_from_all_channels):
+    for index in range(first_index,
+                       len(audio_bytes),
+                       size_of_one_sample_from_all_channels):
         mono_sample = audio_bytes[index:index+wave_file.getsampwidth()]
         yield mono_sample
 
+
 class InvalidWaveChannel(Exception):
-    """Exception for use with working with wav file channels. Raised in the case a channel that doesn't exist is accessed."""
-    
+    """Exception for use with working with wav file channels. Raised\
+    in the case a channel that doesn't exist is accessed."""
+
     pass
